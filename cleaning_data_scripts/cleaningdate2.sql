8d86f465ef75cbc039352a55895639a6b8418dde
saadiak <saadiak@uci.edu>
2021-04-09 17:33:08 -0700

    combining data

--- cleaning_data_scripts/cleaningdate2.sql
+++ cleaning_data_scripts/cleaningdate2.sql
@@ -47,3 +47,59 @@ c."volumetric_soil_water_layer_1", c.total_cloud_cover
 from distinctClimateData c
 left join cleanedFireFreq f on c.time = f.date_trunc and c.city = f.city
 order by c.time
+
+----
+CREATE TABLE housingPopulationCountyCleaned(
+	county VARCHAR,
+	Year int,
+	Density float
+);
+
+CREATE TABLE cityPopulationCleaned(
+	city VARCHAR,
+	Year int,
+	Population float
+);
+UPDATE cityPopulationCleaned
+SET city= replace(city, ' city', '') as city;
+
+UPDATE housingPopulationCountyCleaned
+SET county= replace(county, ' County', '');
+
+SELECT * FROM housingPopulationCountyCleaned
+
+
+SELECT * FROM cityPopulationCleaned
+WHERE city = 'Acton';
+
+SELECT * FROM climateFireFreq
+
+select c.time, c.city, c.county, c.yearonlyclimate, c.frequency, c."2m_temperature", 
+c.total_precipitation, c.low_vegetation_cover, c.high_vegetation_cover, c."10m_wind_speed",
+c."volumetric_soil_water_layer_1", c.total_cloud_cover, hp.density
+from climateFireFreq c
+left join housingPopulationCountyCleaned hp on c.yearonlyclimate = hp.year and c.county = hp.county
+order by c.yearonlyclimate
+
+CREATE VIEW temp as
+select c.time, c.city, c.county, c.yearonlyclimate, c.frequency, c."2m_temperature", 
+c.total_precipitation, c.low_vegetation_cover, c.high_vegetation_cover, c."10m_wind_speed",
+c."volumetric_soil_water_layer_1", c.total_cloud_cover, cp.population
+from climateFireFreq c
+right join cityPopulationCleaned cp on c.yearonlyclimate = cp.year and c.city = cp.city
+order by c.yearonlyclimate
+
+select c.time, c.city, c.county, c.yearonlyclimate, c.frequency, c."2m_temperature", 
+c.total_precipitation, c.low_vegetation_cover, c.high_vegetation_cover, c."10m_wind_speed",
+c."volumetric_soil_water_layer_1", c.total_cloud_cover, c.population populationDensity, hp.density as housingDensitybyCounty
+from temp c
+right join housingPopulationCountyCleaned hp on c.yearonlyclimate = hp.year and c.county = hp.county
+order by c.frequency
+
+
+
+\COPY housingPopulationCountyCleaned from '/Users/saadiakarim 1/Documents/Stats 170a/Final Project Spring/capstone-accenture/clean_data/housingPopulationCountyCleaned.csv' delimiter ',' csv header;
+\COPY cityPopulationCleaned from '/Users/saadiakarim 1/Documents/Stats 170a/Final Project Spring/capstone-accenture/clean_data/cityPopulationCleaned.csv' delimiter ',' csv header;
+
+
+
